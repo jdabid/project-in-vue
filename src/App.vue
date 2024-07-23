0bb58d1088ee9f58fd3b9cf679c7f9af744bf52d
David Castro <dabid.banegas@gmail.com>
2024-07-23 14:44:19 -0500

    1 commit contador

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <h3>Counter: {{counter}}</h3>
+    <h3>Square: {{squareCounter}}</h3>
+
+    <button @click="counter++">+1</button>
+    <button @click="counter--">-1</button>
+  </section>
+</template>
+
+<script setup lang="ts">
+
+import { computed, ref } from 'vue';
+
+const counter = ref(2);
+const squareCounter = computed(()=> counter.value * counter.value);
+</script>
+
