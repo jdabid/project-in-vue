2c3b0d80906787758beab2cff17ad20ec7c75028
David Castro <dabid.banegas@gmail.com>
2024-07-29 14:42:38 -0500

    commit de la aplicacion de chat en vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <template>
-  <h1 className="text-3xl font-bold underline">
-    Aqui tienes una app de vue
-  </h1>
-  <hr>
-
-  <MyCounter :value="1"></MyCounter>
-  <MyCounterScript :value="2"></MyCounterScript>
-  <MyCounterScript2 :value="3"></MyCounterScript2>
+  <IndecisionView></IndecisionView>
 </template>
 
 <script setup lang="ts">
-import MyCounter from '@/components/MyCounter.vue';
-import MyCounterScript from '@/components/MyCounterScript.vue';
-import MyCounterScript2 from '@/components/my-counter-script/MyCounterScript2.vue';
+import IndecisionView from '@/views/IndecisionView.vue';
 </script>
 
---
+++ src/views/IndecisionView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
+    <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
+      <span>Mi esposa</span>
+    </div>
+
+    <!-- Chat Messages -->
+    <ChatMessages :messages="messages"></ChatMessages>
+
+    <!-- MessageBox -->
+    <MessageBox @sendMessage="onMessage"></MessageBox>
+
+
+
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import ChatMessages from '@/components/chat/ChatMessages.vue';
+import MessageBox from '@/components/chat/MessageBox.vue';
+import { useChat } from '@/composables/useChat';
+
+const {messages, onMessage} = useChat();
+
+
+</script>
